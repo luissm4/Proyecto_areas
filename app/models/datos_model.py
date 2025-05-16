505127a8d3980c345e631dac72ed98d359eaa414
luissm4 <luismuiol33@gmail.com>
2025-05-16 18:47:11 -0500

    Update datos_model.py

--- app/models/datos_model.py
+++ app/models/datos_model.py
@@ -6,9 +6,13 @@ class AreaProtegida(db.Model):
     __tablename__ = 'areas_protegidas'
 
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    categoria = db.Column(db.String(100))
+    nombre = db.Column(db.String(255))
     departamento = db.Column(db.String(100))
-    municipio = db.Column(db.String(100))
-    area_km2 = db.Column(db.Float)
-    fecha_declaratoria = db.Column(db.String(50))
+    categoria = db.Column(db.String(100))
+    area = db.Column(db.Float)
+
+    def __init__(self, nombre, departamento, categoria, area):
+        self.nombre = nombre
+        self.departamento = departamento
+        self.categoria = categoria
+        self.area = area
