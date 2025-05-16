56e0b1fc14d6e39e0d5cb781809002c44e1e4c93
luissm4 <luismuiol33@gmail.com>
2025-05-16 18:29:09 -0500

    Update datos_controller.py

--- app/controllers/datos_controller.py
+++ app/controllers/datos_controller.py
@@ -1,3 +1,25 @@
-"""
-Profe cindy esto es en la otra entrega 
-"""
+from app.models.datos_model import AreaProtegida, db
+
+def crear_area(data):
+    nueva_area = AreaProtegida(**data)
+    db.session.add(nueva_area)
+    db.session.commit()
+    return nueva_area
+
+def obtener_areas():
+    return AreaProtegida.query.all()
+
+def actualizar_area(id, data):
+    area = AreaProtegida.query.get(id)
+    if area:
+        for key, value in data.items():
+            setattr(area, key, value)
+        db.session.commit()
+    return area
+
+def eliminar_area(id):
+    area = AreaProtegida.query.get(id)
+    if area:
+        db.session.delete(area)
+        db.session.commit()
+    return area
