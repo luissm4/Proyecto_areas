220c3f7dc13cf23752cf2f7f1ce8f2595ac503ef
luissm4 <luismuiol33@gmail.com>
2025-05-16 18:24:07 -0500

    Update datos_model.py

--- app/models/datos_model.py
+++ app/models/datos_model.py
@@ -1,3 +1,14 @@
-"""
-Profe cindy esto es en la otra entrega 
-"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class AreaProtegida(db.Model):
+    __tablename__ = 'areas_protegidas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    categoria = db.Column(db.String(100))
+    departamento = db.Column(db.String(100))
+    municipio = db.Column(db.String(100))
+    area_km2 = db.Column(db.Float)
+    fecha_declaratoria = db.Column(db.String(50))
