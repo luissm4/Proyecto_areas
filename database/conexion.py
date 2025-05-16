14f9ba82e1f5a17a94afeceae8340115d86babee
luissm4 <luismuiol33@gmail.com>
2025-05-16 18:48:52 -0500

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -1,5 +1,15 @@
-import sqlite3
-from config.settings import DB_PATH 
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from app.models.datos_model import db
 
-def obtener_conexion():
-    return sqlite3.connect(DB_PATH)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:tu_contraseña@localhost/nombre_basedatos'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
