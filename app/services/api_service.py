b8404b9c7d27d894272a70e334ff959e283147ea
luissm4 <luismuiol33@gmail.com>
2025-05-16 19:01:06 -0500

    Update api_service.py

--- app/services/api_service.py
+++ app/services/api_service.py
@@ -1,11 +1,5 @@
-import requests
+from app.controllers.area_dao import guardar_area
 
-class APIClient:
-    API_URL = "https://www.datos.gov.co/resource/xpgq-dbtk.json"
-
-    def obtener_datos(self, limite=100):
-        response = requests.get(f"{self.API_URL}?$limit={limite}")
-        response.raise_for_status()
-        return response.json()
-
-        
+def procesar_datos_api(datos_api):
+    for item in datos_api:
+        guardar_area(item)
