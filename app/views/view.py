2a17b8067571c97916c858ec77f46d6bfa9dd956
luissm4 <luismuiol33@gmail.com>
2025-05-23 17:19:53 -0500

    Update view.py

--- app/views/view.py
+++ app/views/view.py
@@ -1,3 +1,10 @@
-"""
-Profe cindy esto es en la otra entrega 
-"""
+def mostrar_datos(lista_datos):
+    for idx, dato in enumerate(lista_datos, start=1):
+        print(f"{idx}. Área Protegida: {dato['area_protegida']}")
+        print(f"   Municipio: {dato['municipio']}")
+        print(f"   Área Jurisdicción: {dato['area_declarada_en_la_jurisdiccion']}")
+        print(f"   Área Total: {dato['area_total_ha']}")
+        print(f"   Declaratoria: {dato['declaratoria']}")
+        print(f"   Plan Manejo: {dato['plan_de_manejo_ambiental']}")
+        print(f"   Acuerdo Redelimitación: {dato['no_acuerdo_redelimitacion']}")
+        print("-" * 40)
