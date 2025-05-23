aaacb76de1077511be2bff2364e8cf0c0e2e6858
luissm4 <luismuiol33@gmail.com>
2025-05-23 17:21:48 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,25 @@
-from app.database.conexion import create_app
-from app.services.api_service import obtener_datos_api, procesar_datos_api
+from app.controllers.datos_controller import listar_datos, procesar_datos
 
-app = create_app()
+def mostrar_menu():
+    print("\n=== GESTIÓN DE ÁREAS PROTEGIDAS ===")
+    print("1. Cargar datos desde API y guardarlos en MySQL")
+    print("2. Listar datos desde MySQL")
+    print("3. Salir")
 
-with app.app_context():
-    datos = obtener_datos_api()
-    procesar_datos_api(datos)
+def ejecutar():
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            procesar_datos()
+        elif opcion == "2":
+            listar_datos()
+        elif opcion == "3":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida. Intente nuevamente.")
+
+if __name__ == "__main__":
+    ejecutar()
