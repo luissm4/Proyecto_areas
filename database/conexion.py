26aee3a326990aef75b0c41fbfd16888c9f4879c
luissm4 <luismuiol33@gmail.com>
2025-05-23 17:21:10 -0500

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -1,15 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from app.models.datos_model import db
+import mysql.connector
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:tu_contraseña@localhost/nombre_basedatos'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
-    return app
+def crear_conexion():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="areas_protegidas"
+    )
