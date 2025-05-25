02dcb43d8fa2a540bccb5577a34b29bbfd0184cd
luissm4 <luismuiol33@gmail.com>
2025-05-25 16:40:41 -0500

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -1,9 +1,15 @@
 import mysql.connector
+from mysql.connector import Error
 
-def crear_conexion():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="areas_protegidas"
-    )
+def obtener_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='areas_protegidas'
+        )
+        return conexion
+    except Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
