22509b8c7eb1407b8c788121896efa58bbbd36c2
luissm4 <luismuiol33@gmail.com>
2025-05-25 16:41:40 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,32 @@
-from app.controllers.datos_controller import listar_datos, procesar_datos
+from app.controllers.datos_controller import listar_datos, procesar_datos, crear_dato, actualizar, eliminar
 
-def mostrar_menu():
-    print("\n=== GESTIÓN DE ÁREAS PROTEGIDAS ===")
-    print("1. Cargar datos desde API y guardarlos en MySQL")
-    print("2. Listar datos desde MySQL")
-    print("3. Salir")
+def menu():
+    print("\n=== Sistema de Áreas Protegidas ===")
+    print("1. Cargar datos desde la API")
+    print("2. Listar datos")
+    print("3. Crear nuevo dato")
+    print("4. Actualizar dato")
+    print("5. Eliminar dato")
+    print("6. Salir")
 
 def ejecutar():
     while True:
-        mostrar_menu()
+        menu()
         opcion = input("Seleccione una opción: ")
-
         if opcion == "1":
             procesar_datos()
         elif opcion == "2":
             listar_datos()
         elif opcion == "3":
-            print("Saliendo del programa...")
+            crear_dato()
+        elif opcion == "4":
+            actualizar()
+        elif opcion == "5":
+            eliminar()
+        elif opcion == "6":
             break
         else:
-            print("Opción inválida. Intente nuevamente.")
+            print("Opción no válida.")
 
 if __name__ == "__main__":
     ejecutar()
