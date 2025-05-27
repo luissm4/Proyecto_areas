a77efab532f3c96bf62d5cfb0e5ca9cb86e4bbc8
luissm4 <luismuiol33@gmail.com>
2025-05-27 16:01:28 -0500

    Update view.py

--- app/views/view.py
+++ app/views/view.py
@@ -1,6 +1,6 @@
 def mostrar_datos(data):
     for item in data:
-        print(f"{item['no']} - {item['area_protegida']} - {item['municipio']} - {item['area_declarada_en_la_jurisdiccion']} - {item.get('declaratoria',)} - {item.get('plan_de_manejo_ambiental')}")
+        print(f"{item['no']} - {item['area_protegida']} - {item['tipo_area']} - {item['ubicacion_latitud_longitud']} - {item.get('invasiones_previas',)} - {item.get('año_ultima_invasion')} - {item['proximidad_urbana']} - {item['nivel_alerta']}")
 
 def mostrar_mensaje(msg):
     print(msg)
@@ -8,9 +8,11 @@ def mostrar_mensaje(msg):
 def pedir_dato():
     return {
         "no": input("no: "),
-        "area_protegida": input("Área protegida: "),
-        "municipio": input("Municipio: "),
-        "area_declarada_en_la_jurisdiccion": input("Área declarada: "),
-        "declaratoria": input("Declaratoria: "),
-        "plan_de_manejo_ambiental": input("Plan de manejo ambiental: ")
+        "area_protegida": input("area_protegida: "),
+        "tipo_area": input("tipo_area: "),
+        "ubicacion_latitud_longitud": input("ubicacion_latitud_longitud: "),
+        "invasiones_previas": input("invasiones_previas: "),
+        "año_ultima_invasion": input("año_ultima_invasion: "),
+        "proximidad_urbana": input("proximidad_urbana: "),
+        "nivel_alerta": input("nivel_alerta: ")
     }
