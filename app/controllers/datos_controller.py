8b7a98d9f746f29166726275a79a38189842d085
luissm4 <luismuiol33@gmail.com>
2025-05-27 15:57:54 -0500

    Update datos_controller.py

--- app/controllers/datos_controller.py
+++ app/controllers/datos_controller.py
@@ -1,71 +1,29 @@
-from database.conexion import obtener_conexion
+from app.models.datos_model import guardar_datos, obtener_datos, crear_dato_manual, actualizar_dato, eliminar_dato
+from app.services.api_service import obtener_datos_api
+from app.views import view
 
-def guardar_datos(data):
-    conexion = obtener_conexion()
-    cursor = conexion.cursor()
-    for item in data:
-        cursor.execute("""
-            INSERT INTO areas_protegidas (no, area_protegida, municipio, area_declarada_en_la_jurisdiccion,declaratoria, plan_de_manejo_ambiental)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE
-            area_protegida=VALUES(area_protegida),
-            municipio=VALUES(municipio),
-            area_declarada_en_la_jurisdiccion=VALUES(area_declarada_en_la_jurisdiccion),
-            declaratoria=VALUES(declaratoria),
-            plan_de_manejo_ambiental=VALUES(plan_de_manejo_ambiental)
-        """, (
-            item.get("no"),
-            item.get("area_protegida"),
-            item.get("municipio"),
-            item.get("area_declarada_en_la_jurisdiccion"),
-            item.get("declaratoria"),
-            item.get("plan_de_manejo_ambiental")
-        )
-        )
-    conexion.commit()
-    cursor.close()
-    conexion.close()
+def procesar_datos():
+    datos = obtener_datos_api()
+    guardar_datos(datos)
+    view.mostrar_mensaje("Datos guardados.")
 
-def obtener_datos():
-    conexion = obtener_conexion()
-    cursor = conexion.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM areas_protegidas")
-    resultados = cursor.fetchall()
-    cursor.close()
-    conexion.close()
-    return resultados
+def listar_datos():
+    datos = obtener_datos()
+    view.mostrar_datos(datos)
 
-def crear_dato_manual(dato):
-    conexion = obtener_conexion()
-    cursor = conexion.cursor()
-    cursor.execute("""
-        INSERT INTO areas_protegidas (no, area_protegida, municipio, area_declarada_en_la_jurisdiccion, declaratoria, plan_de_manejo_ambiental)
-        VALUES (%s, %s, %s, %s, %s, %s)
-    """, (
-        dato["no"],
-        dato["area_protegida"],
-        dato["municipio"],
-        dato["area_declarada_en_la_jurisdiccion"],
-        dato["declaratoria"],
-        dato["plan_de_manejo_ambiental"]
-    ))
-    conexion.commit()
-    cursor.close()
-    conexion.close()
+def crear_dato():
+    nuevo_dato = view.pedir_dato()
+    crear_dato_manual(nuevo_dato)
+    view.mostrar_mensaje("creado.")
 
-def actualizar_dato(no, campo, valor):
-    conexion = obtener_conexion()
-    cursor = conexion.cursor()
-    query = f"UPDATE areas_protegidas SET {campo} = %s WHERE no = %s"
-    cursor.execute(query, (valor, no))
-    conexion.commit()
-    cursor.close()
-    conexion.close()
+def actualizar():
+    no = input("Ingrese el número del registro qeu va actualizar: ")
+    campo = input("Escriba el campo a actualizar(ej: tipo_area): ")
+    valor = input("Nuevo valor: ")
+    actualizar_dato(no, campo, valor)
+    view.mostrar_mensaje("actualizado.")
 
-def eliminar_dato(no):
-    conexion = obtener_conexion()
-    cursor = conexion.cursor()
-    cursor.execute("DELETE FROM areas_protegidas WHERE no = %s", (no,))
-    conexion.commit()
-    cursor.close()
-    conexion.close()
+def eliminar():
+    no = input("Ingrese el número del registro que va eliminar: ")
+    eliminar_dato(no)
+    view.mostrar_mensaje("eliminado.")
