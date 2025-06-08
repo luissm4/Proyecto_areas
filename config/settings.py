d38e7cfe69f353084917818ea02e51c4122d267b
luissm4 <luismuiol33@gmail.com>
2025-06-07 22:15:51 -0500

    Delete config directory

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'areas_protegidas'
-}
-
-API_URL = 'https://raw.githubusercontent.com/luissm4/datos-areas-protegidas/refs/heads/main/areas_protegidas.json'
