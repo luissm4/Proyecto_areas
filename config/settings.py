8105e6ce51193d6bcdc7396a72adb4c410b4f047
luissm4 <luismuiol33@gmail.com>
2025-06-13 13:59:16 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+# Configuración de la conexión a la base de datos MySQL
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
@@ -5,4 +6,5 @@ DB_CONFIG = {
     'database': 'areas_protegidas'
 }
 
+# URL de la API (en este caso, un archivo JSON en GitHub) desde donde se obtienen los datos
 API_URL = 'https://raw.githubusercontent.com/luissm4/datos-areas-protegidas/refs/heads/main/areas_protegidas.json'
