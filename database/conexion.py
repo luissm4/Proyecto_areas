2ffddb82ec5e0cb04f2e7f08543069369ed6ac98
luissm4 <luismuiol33@gmail.com>
2025-06-13 13:59:59 -0500

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -1,5 +1,7 @@
+# Importa el conector de MySQL para Python
 import mysql.connector
 from config.settings import DB_CONFIG
 
+# Importa la configuración de conexión desde el archivo de settings centralizado
 def obtener_conexion():
     return mysql.connector.connect(**DB_CONFIG)
